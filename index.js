e55e858e0ebcf1be4dbb411e0b9a40debba8d1b0
roanscriven-zamaqo <roan.scriven@zamaqo.com>
2026-02-05 12:27:23 +0200

    Added Bot Commands and Bot Logic

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const express = require("express");
+const express = require('express')
+const expressApp = express()
+const axios = require("axios");
 const path = require("path")
-
 const port = process.env.PORT || 3000;
+expressApp.use(express.static('static'))
+expressApp.use(express.json());
+require('dotenv').config();
 
-const app = express();
+const { Telegraf } = require('telegraf');
 
-app.use(express.static('static'))
-app.use(express.json());
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-app.get("/api/data", (req, res) => {
-  res.status(200).json({
-    data: [
-      { id: 1, title: "Some data" },
-      { id: 2, title: "Some other data" },
-    ],
-  });
-});
-
-app.get("/", (req, res) => {
+expressApp.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get("/index", (req, res) => {
-  res.redirect("/")
-});
+bot.command('start', ctx => {
+  console.log(ctx.from)
+  bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it', {
+  })
+})
 
+bot.command('ethereum', ctx => {
+  var rate;
+  console.log(ctx.from)
+  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`)
+  .then(response => {
+    console.log(response.data)
+    rate = response.data.ethereum
+    const message = `Hello, today the ethereum price is ${rate.usd}USD`
+    bot.telegram.sendMessage(ctx.chat.id, message, {
+    })
+  })
+})
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+bot.launch()
